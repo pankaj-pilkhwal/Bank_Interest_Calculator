31ebccbe650a30104c447bdcd170693633dff139
Pankaj Singh Pilkhwal <pankajpilkhwal01@gmail.com>
2022-04-24 00:50:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-function calculate_all() {
-    calculate1();
-    calculate2();
-    calculate3();
-    calculate4();
-    calculate5();
-}
-function calculate1() {
-    let bank_balance = Number(document.getElementById("bank_balance_1").value)
-    document.getElementById("bank_balance_display_1").value = interest_calculator(bank_balance)
+function calculate_all(){
+    let input_balance = document.getElementsByClassName("input_balance");
+    let output_balance = document.getElementsByClassName("output_balance");
 
-}
-function calculate2() {
-    let bank_balance = Number(document.getElementById("bank_balance_2").value)
-    document.getElementById("bank_balance_display_2").value = interest_calculator(bank_balance)
+    for(let i = 0; i < input_balance.length; i++) {
+        let bank_balance = Number(input_balance[i].value);
+        
+        if(bank_balance > 200000){
+            bank_balance += bank_balance * .1;
+            bank_balance -= bank_balance * .002;
+        }else if(bank_balance > 100000){
+            bank_balance += bank_balance * .05;
+            bank_balance -= bank_balance * .0005;
+        }else {
+            bank_balance += bank_balance * .03;
+            bank_balance -= bank_balance * .002;
+        }
 
-}
-function calculate3() {
-    let bank_balance = Number(document.getElementById("bank_balance_3").value)
-    document.getElementById("bank_balance_display_3").value = interest_calculator(bank_balance)
-
-}
-function calculate4() {
-    let bank_balance = Number(document.getElementById("bank_balance_4").value)
-    document.getElementById("bank_balance_display_4").value = interest_calculator(bank_balance)
-
-}
-function calculate5() {
-    let bank_balance = Number(document.getElementById("bank_balance_5").value)
-    document.getElementById("bank_balance_display_5").value = interest_calculator(bank_balance)
-}
-
-function interest_calculator(bank_balance){
-    if(bank_balance > 200000){
-        bank_balance += bank_balance * .1;
-        bank_balance -= bank_balance * .002;
-    }else if(bank_balance > 100000){
-        bank_balance += bank_balance * .05;
-        bank_balance -= bank_balance * .0005;
-    }else {
-        bank_balance += bank_balance * .03;
-        bank_balance -= bank_balance * .002;
+        output_balance[i].value = bank_balance;
+    
     }
-
-    return bank_balance;
 }
